4b990b4fdcbd2708e6c0f834beb63374c80b5d1f
Teacher1 <Teacher1@CLASS1>
2019-01-18 15:14:29 +0330

    initial project

---
+++ Backend/Academy.Gateways.RestApi/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academy.Gateways.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world  from mvc controller");
+        }
+    }
+}
