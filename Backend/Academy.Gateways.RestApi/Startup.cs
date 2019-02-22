50758dfa4b2eec5697972f90496bad60d702d6ca
Teacher1 <Teacher1@CLASS1>
2019-02-22 14:51:47 +0330

    access token validation added to rest api

--- Backend/Academy.Gateways.RestApi/Startup.cs
+++ Backend/Academy.Gateways.RestApi/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Academy.Application;
 using Academy.Config;
 using Autofac;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Academy.Gateways.RestApi
@@ -16,8 +18,15 @@ namespace Academy.Gateways.RestApi
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.Filters.Add(new AuthorizeFilter()));
             services.AddCors();
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.ApiName = "soudabeh-api";
+                    options.RequireHttpsMetadata = false;
+                });
         }
         public void ConfigureContainer(ContainerBuilder builder)
         {
@@ -30,6 +39,7 @@ namespace Academy.Gateways.RestApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseCors(builder =>
             {
                 //TODO: don't try this at home
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserManager } from 'oidc-client';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'academy';
+  
+  
+
+  login() {
+    const settings = {
+      authority: 'http://localhost:5000/',
+      client_id: 'academy-soudabeh',
+      redirect_uri: 'http://localhost:4200/auth-callback',
+      response_type: "id_token token",
+      scope: "openid profile soudabeh-api",
+      loadUserInfo: true,
+      filterProtocolClaims: true,
+    }
+    const userManager =new UserManager(settings);
+    userManager.signinRedirect();
+  }
 }
